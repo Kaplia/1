3633ca8825c70e43c8952cc8221c2246aa612785
Nadia Kaplia <kapelkanadia@gmail.com>
2019-11-25 15:07:49 +0200

    2 lesson. edits

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 let completedTasks = document.querySelector('.completed-tasks').innerHTML;
 let openTasks = document.querySelector('.open-tasks').innerHTML;
 
-function completeTask () {
-	if (confirm("Are you sure you want to change the number of tasks?")) {
-		if (openTasks != 0) {
-			openTasks--;
-			completedTasks++;
-		}
-		document.querySelector('.completed-tasks').innerHTML = completedTasks;
-		document.querySelector('.open-tasks').innerHTML = openTasks;
-		
-    } else {
-        alert("Cancel")
+function completeTask() {
+    if (openTasks != 0) {
+        if (confirm("Are you sure you want to change the number of tasks?")) {
+            openTasks--;
+            completedTasks++;
+        } else {
+            alert("Cancel")
+        }
+        document.querySelector('.completed-tasks').innerHTML = completedTasks;
+        document.querySelector('.open-tasks').innerHTML = openTasks;
     }
 }
 
-let image = document.querySelector('.notifications').innerHTML;
-
-var arr = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg', 'image-4.jpg'];
-
-function imageClick (id) {
-	document.querySelector('.notifications').innerHTML = id;
+function imageClick(id) {
+    document.querySelector('.notifications').innerHTML = id;
 }
